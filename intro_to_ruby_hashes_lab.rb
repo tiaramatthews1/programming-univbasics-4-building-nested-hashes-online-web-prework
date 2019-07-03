d163fa3fcbeae5e15fa07e19389b8680a0e8e8b3
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-03 01:35:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,18 +12,18 @@ def monopoly_with_second_tier
  end
 
 def monopoly_with_third_tier
-:one_piece_owned =>
-:two_pieces_owned =>
-:three_pieces_owned =>
-:four_pieces_owned =>
+:one_piece_owned => {]
+:two_pieces_owned => {}
+:three_pieces_owned => {}
+:four_pieces_owned => {}
 	 
 end
 
 def monopoly_with_fourth_tier
-  :reading_railroad =>
-  :pennsylvania_railroad =>
-  :b_and_o_railroad =>
-  :shortline =>
+  :reading_railroad => {}
+  :pennsylvania_railroad => {}
+  :b_and_o_railroad => {}
+  :shortline => {}
 	
 
 end
