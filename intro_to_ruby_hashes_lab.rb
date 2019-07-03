729a3323460d57d1f5880cfd0b3f5254c6305b02
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-03 01:54:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,13 @@ def monopoly_with_second_tier
  end
 
 def monopoly_with_third_tier
-
+monopoly = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :},
+      :names => {}
+  }
+	  }
 end
 
 def monopoly_with_fourth_tier
