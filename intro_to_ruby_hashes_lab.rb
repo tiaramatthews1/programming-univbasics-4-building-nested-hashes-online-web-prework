05d6ce44b42d320f56b6de0590886583121d5834
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-03 02:09:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ monopoly = {
         :one_piece_owned => 25, 
         :two_pieces_owned => 50, 
         :three_pieces_owned => 100, 
-        :four_pieces_owned => 200}
+        :four_pieces_owned => 200
       }
   }
 	  }
